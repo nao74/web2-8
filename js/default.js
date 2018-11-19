6c0debd49d5cf60b9d9f45149be7bb2ca1c96fa9
nao74 <38421007+nao74@users.noreply.github.com>
2018-11-19 17:12:48 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -15,7 +15,7 @@ var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime()-now.getTime();
 var counter = separate_time(now);
 document.getElementById('countdown').textContent =
-  '東京オリンピックまであと'+
+  '東京オリンピックまであと ' +
   counter[3] + '日' +
   counter[2] + '時' +
   counter[1] + '分' +
